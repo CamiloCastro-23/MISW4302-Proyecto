02e7ac1fe36bc6f5c6ca4a4c2a7bcb74ee1ea588
CamiloCastro-MISO <bc.castro@uniandes.edu.co>
2024-09-19 19:33:38 -0500

    adding routes por pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './alarm-app/pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./alarm-app/alarm-app.module').then(m => m.AlarmAppModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
